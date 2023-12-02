d7056555f2ee43f58d01120dbed80fd7ebf2d730
veliuysal <veliuysaldev@gmail.com>
2023-12-02 13:03:57 +0300

    spring boot ui

--- spring_boot/carlist-byyear.js
+++ spring_boot/carlist-byyear.js
@@ -41,3 +41,48 @@ function updateTable2(data) {
            tableBody.appendChild(row);
        });
 }
+
+
+fetch("http://localhost:8080/api/car?year=1968")
+.then(response => {
+    if (!response.ok) {
+        throw new Error('API isteği başarısız. Durum Kodu: ' + response.status);
+    }
+    return response.json();
+})
+.then(data => {
+    updateTable3(data);
+})
+.catch(error => {
+    console.error(error);
+});
+
+
+function updateTable3(data) {
+    console.log("response :" + data)
+    console.log("response :" + data.id)
+
+    const tableBody = document.getElementById('table-body-3');
+    const row = document.createElement('tr');
+   /* row.innerHTML = `
+      <td>${data.id}</td>
+      <td>${data.brand}</td>
+      <td>${data.model}</td>
+      <td>${data.year}</td>
+    `;
+
+    tableBody.appendChild(row);*/
+
+    // API'den gelen verileri döngü ile tabloya ekleyin
+       data.forEach(item => {
+           const row = document.createElement('tr');
+           row.innerHTML = `
+         <td>${item.id}</td>
+         <td>${item.brand}</td>
+         <td>${item.model}</td>
+         <td>${item.year}</td>
+       `;
+   
+           tableBody.appendChild(row);
+       });
+}
