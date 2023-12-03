0ab816b2458897c6b2cb60b2a9aede074961e2b4
veliuysal <veliuysaldev@gmail.com>
2023-12-03 13:49:57 +0300

    ...

--- spring_boot/carList.js
+++ spring_boot/carList.js
@@ -1,16 +1,16 @@
 fetch("http://localhost:8080/api/car/all")
-.then(response => {
-    if (!response.ok) {
-        throw new Error('API isteği başarısız. Durum Kodu: ' + response.status);
-    }
-    return response.json();
-})
-.then(data => {
-    updateTable(data);
-})
-.catch(error => {
-    console.error(error);
-});
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('API isteği başarısız. Durum Kodu: ' + response.status);
+        }
+        return response.json();
+    })
+    .then(data => {
+        updateTable(data);
+    })
+    .catch(error => {
+        console.error(error);
+    });
 
 
 function updateTable(data) {
@@ -19,26 +19,33 @@ function updateTable(data) {
 
     const tableBody = document.getElementById('table-body');
     const row = document.createElement('tr');
-   /* row.innerHTML = `
-      <td>${data.id}</td>
-      <td>${data.brand}</td>
-      <td>${data.model}</td>
-      <td>${data.year}</td>
-    `;
-
-    tableBody.appendChild(row);*/
+    /* row.innerHTML = `
+       <td>${data.id}</td>
+       <td>${data.brand}</td>
+       <td>${data.model}</td>
+       <td>${data.year}</td>
+     `;
+ 
+     tableBody.appendChild(row);*/
 
     // API'den gelen verileri döngü ile tabloya ekleyin
-       data.forEach(item => {
-           const row = document.createElement('tr');
-           row.innerHTML = `
+    data.forEach(item => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
          <td>${item.id}</td>
          <td>${item.brand}</td>
          <td>${item.model}</td>
          <td>${item.year}</td>
+         <td>
+         <button onclick="guncelle( ${item} )">Güncelle</button>
+         </td>
        `;
-   
-           tableBody.appendChild(row);
-       });
+
+        tableBody.appendChild(row);
+    });
+}
+
+function guncelle(item) {
+    console.log("Güncelle", item)
 }
 
